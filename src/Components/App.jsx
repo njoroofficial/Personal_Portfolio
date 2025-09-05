46a5c1193e16f47432c9bd3c747e4918aa0e4ff6
njoroofficial <mosesn579@gmail.com>
2025-09-05 21:06:49 +0300

    feat: Impliment a hero section

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./Header";
+import Hero from "./Hero";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-background">
       <Header />
+      <main>
+        <Hero />
+      </main>
     </div>
   );
 }
