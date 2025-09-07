0cf729c2e3f63582b490431816713c3a16b93dc4
njoroofficial <mosesn579@gmail.com>
2025-09-07 12:43:25 +0300

    React mapping navigation buttons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,27 +1,22 @@
 // Header section component
 
 export default function Header() {
+  // navbar buttons
+  const navbar_Buttons = ["Home", "About", "Skills", "Blog", "Contact"];
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-md shadow-lg">
       <div className="container px-4 py-4 mx-auto">
         <div className="flex items-center justify-between">
           <div className="text-2xl font-bold text-primary">Moses Njoroge</div>
           <nav className="flex items-center text-primary gap-8">
-            <button className="text-muted-foreground hover:text-primary transition-colors">
-              Home
-            </button>
-            <button className="text-muted-foreground hover:text-primary transition-colors">
-              About
-            </button>
-            <button className="text-muted-foreground hover:text-primary transition-colors">
-              Skills
-            </button>
-            <button className="text-muted-foreground hover:text-primary transition-colors">
-              Blog
-            </button>
-            <button className="text-muted-foreground hover:text-primary transition-colors">
-              Contact
-            </button>
+            {/* Mapping navbar buttons */}
+            {navbar_Buttons.map((navbar_Button) => {
+              return (
+                <button className="text-muted-foreground hover:text-primary transition-colors">
+                  {navbar_Button}
+                </button>
+              );
+            })}
           </nav>
         </div>
       </div>
